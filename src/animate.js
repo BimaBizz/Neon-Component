e85655ef5fbfd7734176123bc28b446d3a8f92ad
Bima Mahendra <mahendrabima278@gmail.com>
2023-03-07 20:21:51 +0700

    Up Code

---
+++ src/animate.js
@@ -0,0 +1,37 @@
+ScrollReveal({
+    reset: false,
+    distance: '60px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.nav div, .items a, .subs div, .icons a', {
+    delay: 500,
+    origin: 'bottom',
+    interval: 200
+});
+
+ScrollReveal().reveal('.gambaran', {
+    delay: 500,
+    origin: 'left',
+    interval: 200
+});
+
+ScrollReveal().reveal('.intro h1', {
+    delay: 600,
+    origin: 'left',
+});
+
+ScrollReveal().reveal('.image, .intro div, .isian', {
+    delay: 200,
+    origin: 'right',
+});
+
+ScrollReveal().reveal('.title', {
+    delay: 600,
+    origin: 'top',
+});
+ScrollReveal().reveal('.intro p', {
+    delay: 400,
+    origin: 'bottom',
+});
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./*.html"
+  ],
+  theme: {
+    fontFamily: {
+      "pop": 'Poppins',
+      "kala": 'Kalam',
+      "LED": 'NEONLED',
+      "LITE": 'Lite',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
